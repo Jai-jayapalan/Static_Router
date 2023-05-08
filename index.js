3a50f9465710f715edd4db790944db5a4e4c7f2d
Jai_Jayathilak <jaijayathilak@gmail.com>
2023-05-08 20:47:08 +0530

    form created

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { constants } = require('buffer');
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -45,8 +46,74 @@ app.get('/service/:id', (req, res, next) => {
     res.render('index', {data});
     next()
 });
+app.get("/new", (req,res)=>{
+    res.send (`
+    <html>
+        <head>
+            <title>
+                sign-up page
+            </title>
+            <style>
+        body{
+            display: block;
+            margin-top: 50px;
+            heigth : 50px;
+        }
+        .container{
+            display: flex;
+            justify-content: center;
+            margin-left : 450px;
+            margin-top : 150px;
+            width : 400px;
+            border: 2px solid black;
+            border-radius: 10px;
+            padding: 10px ;
+            box-shadow: 7px 10px 5px gray;
+        }
+        form{
+            border: 2px solid black;
+            border-radius: 10px;
+            padding : 20px;
+        }
+        input{
+            margin-bottom : 10px;
+        }
+        button{
+            margin-left : 80px;
+        }
+        h1{
+            text-align: center;
+        }
+    </style>
+        </head>
+        <body>
+        <div class = 'container'>
+            <div class='content'>
+            <h1> user details </h1>
+                <form method='post' action='/answer'>
+                    name:<input type='text',placeholder='enter your name' name='user' /><br>
+                    age:<input type='number',placeholder='enter your age' name='age' /><br>
+                    <button>submit</button>
+                </form>
+            </div> 
+            </div>  
+        </body>
+    </html>
+    `)
+})// port listening.....
 
-// port listening.....
+app.post("/answer", (req,res)=>{
+    const name = req.body.user
+    const age = req.body.age
+    const data1 = [
+        Name = name,
+        Age = age
+    ]
+    res.render('form',{data1})
+})// port listening.....
 app.listen(3000, () => {
   console.log('App gets started to running on the port 3000');
 });
+
+
+
